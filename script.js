527410211ab4c77dddee0167e29f033005d5f738
Yegor Dyakovskiy <yegor.dyakovskiy@aiesec.net>
2023-04-19 19:25:28 +0600

    fear: add elements in html, css, sript files

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const buttons = document.querySelectorAll(".keyboard button");
+const output = document.querySelector(".output");
+console.log(output);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.getAttribute("value");
+    if (value === "space") {
+      output.value += " ";
+    } else if (value === "backspace") {
+      output.value = output.value.slice(0, -1);
+    } else {
+      output.value += value;
+    }
+  });
+});
