8da1568be68e819cdc0397a4c28e1c40786f4eb6
Yegor Dyakovskiy <yegor.dyakovskiy@aiesec.net>
2023-04-20 08:56:22 +0600

    feat: add new elements and styles

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 "use strict";
-
+const body = document.querySelector("body");
+//body.innerHTML = ``; это чтобы динамически подгрузить контент, в нужный момент подключить
 const buttons = document.querySelectorAll(".keyboard button");
 const output = document.querySelector(".output");
+const backspaceButton = document.querySelector(".backspace");
+const delButton = document.querySelector(".del");
+const altButton = document.querySelector(".alt");
+const capslockButton = document.querySelector(".capslock");
+const shiftButton = document.querySelector(".shift");
+const ctrlButton = document.querySelector(".ctrl");
+console.log(backspaceButton);
 console.log(output);
 
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.getAttribute("value");
+    if (value === "space") {
+      output.value += " ";
+    } else if (value === "tab") {
+      output.value += "    ";
+    } else if (value === "enter") {
+      output.value += "\n";
+    } else if (
+      value === "backspace" ||
+      value === "ctrl" ||
+      value === "shift" ||
+      value === "win" ||
+      value === "alt" ||
+      value === "capslock" ||
+      value === "del"
+    ) {
+      return 0;
+    } else {
+      output.value += value;
+    }
+  });
+});
+
+//первый вариант - пишет все буквы, а backspase удаляет последнюю букву, но не перед курсором. а именно последнюю
+/*
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.getAttribute("value");
@@ -15,4 +50,4 @@ buttons.forEach((button) => {
       output.value += value;
     }
   });
-});
+}); */
